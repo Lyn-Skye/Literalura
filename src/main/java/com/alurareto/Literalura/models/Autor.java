91bd2d14f2aa756db2c559d9e54fb9a6877e9aaf
Hilda Lacaze <qfbhildalacaze@gmail.com>
2024-07-03 00:20:46 -0500

    a) Agregada el enlace many to one con Libro.java b) Agregado el constructor vacío c) Agregado el campo ID, así como su get y set

--- src/main/java/com/alurareto/Literalura/models/Autor.java
+++ src/main/java/com/alurareto/Literalura/models/Autor.java
@@ -1,11 +1,21 @@
 package com.alurareto.Literalura.models;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "autores")
 public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    @Column (unique = true)
     private String nombre;
     private String fechaDeNacimiento;
     private String fechaDeMuerte;
+    @ManyToOne
     private Libro libro;
+
+    public Autor(){}
 
     public Autor(DatosAutor datosAutor){
         this.nombre = datosAutor.nombre();
@@ -48,4 +58,8 @@ public class Autor {
     public void setLibro(Libro libro) {
         this.libro = libro;
     }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
 }
