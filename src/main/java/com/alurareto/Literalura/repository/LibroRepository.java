d9c9c7e8b69c7e3196fddb737721000ec6f05b0c
Hilda Lacaze <qfbhildalacaze@gmail.com>
2024-07-03 00:26:41 -0500

    a) Agregada la funcion de encontrar por idiomas b) Se añadió el @Repository para que la clase funcione con la base de datos

--- src/main/java/com/alurareto/Literalura/repository/LibroRepository.java
+++ src/main/java/com/alurareto/Literalura/repository/LibroRepository.java
@@ -1,13 +1,14 @@
 package com.alurareto.Literalura.repository;
 
-import com.alurareto.Literalura.models.Autor;
 import com.alurareto.Literalura.models.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LibroRepository extends JpaRepository<Libro,Long> {
 
+    List<Libro> findByIdiomas(String idioma);
 
 }
