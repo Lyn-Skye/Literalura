47fa92bf9a8f400f457e2eb4a537e48ab99a4a26
Hilda Lacaze <qfbhildalacaze@gmail.com>
2024-07-03 00:23:36 -0500

    a) Agregada la variable de contador de descargas

--- src/main/java/com/alurareto/Literalura/models/DatosLibro.java
+++ src/main/java/com/alurareto/Literalura/models/DatosLibro.java
@@ -10,6 +10,7 @@ import java.util.List;
 public record DatosLibro(
         @JsonAlias("title") String titulo,
         @JsonAlias("authors") List<DatosAutor> autores,
-        @JsonAlias("languages") List<String> idiomas) {
+        @JsonAlias("languages") List<String> idiomas,
+        @JsonAlias("download_count") String descargas) {
 
 }
