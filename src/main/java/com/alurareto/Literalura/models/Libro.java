6e91354fa1719b7c3a4add24c5418ac44ba93c3d
Hilda Lacaze <qfbhildalacaze@gmail.com>
2024-07-03 00:25:28 -0500

    a) Agregados los parametros para que funcione en la base de datos, el ID y el mapeo One To Many para enlazar a Autores.java b) Agregado el constructor vacio c) Agregados los setter y getters de descargas y id,

--- src/main/java/com/alurareto/Literalura/models/Libro.java
+++ src/main/java/com/alurareto/Literalura/models/Libro.java
@@ -1,22 +1,36 @@
 package com.alurareto.Literalura.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+
+@Entity
+@Table(name = "libros")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String titulo;
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Autor> autores;
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<String> idiomas;
+    private String descargas;
+
+    Libro(){}
 
     public Libro(DatosLibro datosLibro){
         this.titulo = datosLibro.titulo();
         this.idiomas = datosLibro.idiomas();
+        this.descargas = datosLibro.descargas();
     }
 
     public String toString() {
-        return "Titulo: " + titulo + ", Autores: " + autores + ", Lenguajes: " + idiomas +"}";
+        return "Titulo: " + titulo + ", Autores: " + autores + ", Lenguajes: " + idiomas + ", Número de veces descargado: "+ descargas + "}";
     }
 
     public String getTitulo() {
@@ -42,6 +56,22 @@ public class Libro {
 
     public void setIdiomas(List<String> idiomas) {
         this.idiomas = idiomas;
+    }
+
+    public String getDescargas() {
+        return descargas;
+    }
+
+    public void setDescargas(String descargas) {
+        this.descargas = descargas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
 
